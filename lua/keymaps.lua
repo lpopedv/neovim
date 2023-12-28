a8714b092f67fa4b6e7a2719807dba1a63ad4056
Lucas Pope <lpopedv@proton.me>
2023-12-27 21:33:59 -0300

    new general keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,10 @@
 -- vim mapleader
 vim.g.mapleader = " "
 
+-- general keymaps
+vim.keymap.set('n', '<leader>vp', ':vsplit<CR>')
+vim.keymap.set('n', '<leader>q', ':q<CR>')
+
 -- lspconfig keymaps
 vim.keymap.set('n', 'I', vim.lsp.buf.hover, {})
 vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
