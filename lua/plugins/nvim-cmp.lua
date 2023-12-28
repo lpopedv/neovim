501f04a27662e0726fcb18870e40445cfec7afdf
Lucas Pope <lpopedv@proton.me>
2023-12-28 00:01:06 -0300

    add github copilot plugin

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,16 +1,26 @@
 return {
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/cmp-vsnip',
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-vsnip",
 
-  config = function()
-    local cmp = require('cmp')
+	config = function()
+		local cmp = require("cmp")
 
-    cmp.setup({
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'buffer' },
-      },
-    })
-  end
+		cmp.setup({
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "tsserver" },
+				{ name = "buffer" },
+			},
+			snippet = {
+				expand = function(args)
+					vim.fn["vsnip#anonymous"](args.body)
+				end,
+			},
+			mapping = {
+				["<C-Space>"] = cmp.mapping.select_prev_item(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			},
+		})
+	end,
 }
